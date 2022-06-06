3759c89431cc023429030b6a77d331133b3594fd
mrrocford <95035813+mrrocford@users.noreply.github.com>
2022-06-06 23:35:11 +0300

    Add files via upload

---
+++ auctions/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Lot, LotImage, LotCategory, Bid, Watchlist, Comment
+
+# Register your models here.
+
+
+class LotAdmin(admin.ModelAdmin):
+    list_display = ("lot_name", "lot_price", "lot_description", "lot_date","lot_status", "lot_author", "lot_category", "lot_viewimage", "lot_bid")
+admin.site.register(User, UserAdmin)
+admin.site.register(Lot, LotAdmin)
+admin.site.register(LotImage)
+admin.site.register(Bid)
+admin.site.register(LotCategory)
+admin.site.register(Watchlist)
+admin.site.register(Comment)
